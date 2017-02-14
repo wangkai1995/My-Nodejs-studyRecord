e09f3eeb19f4c54b7b59d7566028e8f9988368b5
wangkai1995 <wwwwkwy@126.com>
2017-02-14 10:01:05 +0800

    Revert "react select input demo"
    This reverts commit 763daf96cd85f849cc518fbe0b88943201d247a7.

--- react-demo/src/app.js
+++ react-demo/src/app.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDom from 'react-dom';
-//DMOE 1 tab
+//DMOE 1
 import {Tabs ,TabNav ,TabContent,TabPane} from'./com/tab';
-//DMOE 2 事件系统
+//DMOE 2
 import {ImgTest} from './com/eventDemo1';
-//DMOE 3 表单
+//DMOE 3
 import {From} from './com/from';
-//DMOE 4 高阶组件
-import MyContainer from './com/higher_fun_1';
-//DMOE 5 组合高阶组件
-import {InputQuery} from './com/higher_fun_2';
+
 
 
 
@@ -39,20 +36,8 @@ function Page(){
 
 
 	/*********DEMO 3**********/
-	// return (
-	// 	<From></From>
-	// )
-	
-
-	/****** Demo 4  *********/
-	// return (
-	// 	<MyContainer test={'test11'} ></MyContainer>
-	// )
-	
-
-	/******** Demo 5 *********/
 	return (
-		<InputQuery></InputQuery>
+		<From></From>
 	)
 
 
