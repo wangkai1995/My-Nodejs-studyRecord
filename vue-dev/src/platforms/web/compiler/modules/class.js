0d9cdf1a6048e5173e5395616d39f9869a942094
wangkai1995 <wwwwkwy@126.com>
2017-06-20 16:34:56 +0800

    vue comPiler

--- vue-dev/src/platforms/web/compiler/modules/class.js
+++ vue-dev/src/platforms/web/compiler/modules/class.js
@@ -7,10 +7,14 @@ import {
   baseWarn
 } from 'compiler/helpers'
 
+//转换Node
 function transformNode (el: ASTElement, options: CompilerOptions) {
+  //拿到警告方法
   const warn = options.warn || baseWarn
+  //从attr中获取class
   const staticClass = getAndRemoveAttr(el, 'class')
   if (process.env.NODE_ENV !== 'production' && staticClass) {
+    //获取表达式 传入class 和分隔符 校验？
     const expression = parseText(staticClass, options.delimiters)
     if (expression) {
       warn(
@@ -21,14 +25,20 @@ function transformNode (el: ASTElement, options: CompilerOptions) {
       )
     }
   }
+  //如果class存在
   if (staticClass) {
+    //赋值过去对应的class
     el.staticClass = JSON.stringify(staticClass)
   }
+  //获取v-bind:class
   const classBinding = getBindingAttr(el, 'class', false /* getStatic */)
   if (classBinding) {
+    //设置对应标志
     el.classBinding = classBinding
   }
 }
+
+
 
 function genData (el: ASTElement): string {
   let data = ''
@@ -40,6 +50,7 @@ function genData (el: ASTElement): string {
   }
   return data
 }
+
 
 export default {
   staticKeys: ['staticClass'],
